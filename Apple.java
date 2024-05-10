6b50d3ccd03f9f92435e3d29c4630f2d0424a09e
FelixZichenHuang <166442323+FelixZichenHuang@users.noreply.github.com>
2024-05-10 16:10:13 -0400

    Add background image, sound of the elephant, idle images of the elephant; Increase the speed of the apple.

---
+++ Apple.java
@@ -0,0 +1,30 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Food for our elephant.
+ * 
+ * @author Felix Huang 
+ * @version May 2024
+ */
+public class Apple extends Actor
+{
+    /**
+     * Act - do whatever the Apple wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Apple falls downwards.
+        setLocation(getX(), getY() + 3);
+        
+        // Remove apple and draw game over when apple gets to bottom.
+        MyWorld world = (MyWorld) getWorld();
+        if (getY() >= world.getHeight())
+        {
+            world.gameOver();
+            world.removeObject(this);
+        }
+    }
+    
+    
+}
---
+++ Elephant.java
@@ -0,0 +1,93 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * The Elephant, our hero.
+ * 
+ * @author Felix Huang
+ * @version May 2024
+ */
+public class Elephant extends Actor
+{
+    GreenfootSound elephantSound = new GreenfootSound("elephantcub.mp3");
+    GreenfootImage[] idleRight = new GreenfootImage[8];
+    GreenfootImage[] idleLeft = new GreenfootImage[8];
+
+    // Direction that the elephant is facing.
+    String facing = "right";
+
+    /**
+     *
+     * Constructor - The code that gets run one time when object is created.
+     */ public Elephant()
+    {
+        for (int i = 0; i < idleRight.length; i++)
+        {
+            idleRight[i] = new GreenfootImage("images/elephant_idle/idle" + i + ".png");
+            idleRight[i].scale(100, 100);
+        }
+
+        for (int i = 0; i < idleLeft.length; i++)
+        {
+            idleLeft[i] = new GreenfootImage("images/elephant_idle/idle" + i + ".png");
+            idleLeft[i].mirrorHorizontally();
+            idleLeft[i].scale(100, 100);
+        }
+
+        setImage(idleRight[0]);
+    }
+
+    /**
+     *
+     * Animate the elephant.
+     */
+    int imageIndex = 0;
+    public void animateElephant()
+    {
+        if (facing.equals("right"))
+        {
+            setImage(idleRight[imageIndex]);
+            imageIndex = (imageIndex + 1) % idleRight.length;
+        }
+        else
+        {
+            setImage(idleLeft[imageIndex]);
+            imageIndex = (imageIndex + 1) % idleLeft.length;
+        }
+    }
+
+    public void act()
+    {
+        if (Greenfoot.isKeyDown("left")) 
+        {
+            facing = "left";
+            move(-6);
+        }
+        else if (Greenfoot.isKeyDown("right")) 
+        {
+            facing = "right";
+            move(6);
+        }
+        
+        // Remove apple if elephant eats it.
+        eat();
+
+        //Animate the elephant.
+        animateElephant();
+    }
+    
+    /**
+     * Eat the apple and spawn new apple if an apple is eaten.
+     * 
+     */
+    public void eat()
+    {
+        if (isTouching(Apple.class))
+        {
+            removeTouching(Apple.class);
+            MyWorld world = (MyWorld) getWorld();
+            world.createApple();
+            world.increaseScore();
+            elephantSound.play();
+        }
+    }
+}
