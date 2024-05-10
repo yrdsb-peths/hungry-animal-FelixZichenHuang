b7f7fca56e4cc6fa65acca3384b394c1049525c4
FelixZichenHuang <166442323+FelixZichenHuang@users.noreply.github.com>
2024-05-10 15:01:40 -0400

    Revert "Add background of the game, sound of the elephant and idle images of the elephant."
    This reverts commit 808bf94c182562140a8c8a0f257c3d62e7481344.

--- Elephant.java
+++ Elephant.java
@@ -8,72 +8,23 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Elephant extends Actor
 {
-    GreenfootSound elephantSound = new GreenfootSound("elephantcub.mp3");
-    GreenfootImage[] idleRight = new GreenfootImage[8];
-    GreenfootImage[] idleLeft = new GreenfootImage[8];
-    
-    // Direction that the elephant is facing.
-    String facing = "right";
-    
     /**
-     * 
-     * Constructor - The code that gets run one time when object is created.
+     * Act - do whatever the Elephant wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
      */
-    public Elephant()
-    {
-        for (int i = 0; i < idleRight.length; i++)
-        {
-            idleRight[i] = new GreenfootImage("images/elephant_idle/idle" + i + ".png");
-            idleRight[i].scale(100, 100);
-        }
-        
-        for (int i = 0; i < idleLeft.length; i++)
-        {
-            idleLeft[i] = new GreenfootImage("images/elephant_idle/idle" + i + ".png");
-            idleLeft[i].mirrorHorizontally();
-            idleLeft[i].scale(100, 100);
-        }
-        
-        setImage(idleRight[0]);
-    }
-    
-    /**
-     * 
-     * Animate the elephant.
-     */
-    int imageIndex = 0;
-    public void animateElephant()
-    {
-        if (facing.equals("right"))
-        {
-            setImage(idleRight[imageIndex]);
-            imageIndex = (imageIndex + 1) % idleRight.length;
-        }
-        else
-        {
-            setImage(idleLeft[imageIndex]);
-            imageIndex = (imageIndex + 1) % idleLeft.length;
-        }
-    }
-    
     public void act()
     {
         if (Greenfoot.isKeyDown("left")) 
         {
-            facing = "left";
-            move(-6);
+            move(-5);
         }
         else if (Greenfoot.isKeyDown("right")) 
         {
-            facing = "right";
-            move(6);
+            move(5);
         }
         
         // Remove apple if elephant eats it.
         eat();
-        
-        //Animate the elephant.
-        animateElephant();
     }
     
     /**
@@ -88,7 +39,6 @@ public class Elephant extends Actor
             MyWorld world = (MyWorld) getWorld();
             world.createApple();
             world.increaseScore();
-            elephantSound.play();
         }
     }
 }
